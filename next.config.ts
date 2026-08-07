907bcda69703a5808ca2f7bb5d93100b1501cb76
jagat-developer <jagat@codepaper.com>
2026-08-07 10:50:51 -0400

    Add flat LP fallbacks with photos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const LP_SLUGS = [
+  'custom-truck-bodies',
+  'service-bodies',
+  'flatbed-dump-bodies',
+  'fleet-upfitting',
+  'van-bodies',
+  'thank-you',
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return LP_SLUGS.map((slug) => ({
+      source: `/lp/${slug}`,
+      destination: `/lp/${slug}.html`,
+    }));
+  },
 };
 
 export default nextConfig;
